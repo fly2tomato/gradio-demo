bbcf84c3d97a46fbaf9ab35f2bd8178a9a4db524
fly2tomato <xuju5220@gmail.com>
2023-02-03 23:30:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from transformers import pipeline
+# from transformers import pipeline
 import gradio as gr
 
 
 def hello(i):
-    classifier = pipeline("sentiment-analysis")
-    a = classifier(i)
-    return a
+#     classifier = pipeline("sentiment-analysis")
+#     a = classifier(i)
+    return "9125->icu"
 
 iface = gr.Interface(fn=hello, inputs="text", outputs="text")
 iface.launch()
